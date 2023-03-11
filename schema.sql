39cc6ad1ac505e775a121c5a910cf7f2772f5e25
beanjo55 <abbyschweitzer22@gmail.com>
2023-03-11 03:35:16 -0500

    add highlights table

--- schema.sql
+++ schema.sql
@@ -16,3 +16,13 @@ CREATE TABLE IF NOT EXISTS movieSuggestions (data TEXT NOT NULL);
 CREATE TABLE IF NOT EXISTS rssFeeds (data TEXT NOT NULL);
 
 CREATE TABLE IF NOT EXISTS recurringReminders (data TEXT NOT NULL);
+
+CREATE TABLE IF NOT EXISTS highlights (
+    user_id BIGINT NOT NULL,
+    guild_id BIGINT NOT NULL,
+    phrase TEXT NOT NULL,
+    snooze_until INT,
+    PRIMARY KEY (guild_id, user_id)
+)
+
+CREATE UNIQUE INDEX IF NOT EXISTS highlights_guild_user_phrase_idx on highlights (guild_id, user_id, phrase)
